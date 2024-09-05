0886d51d1c2f0c422f70e648519e8939a1e61ffa
Berke-aras <berkearastaman@outlook.com>
2024-09-05 18:36:00 +0300

    updt

--- app.py
+++ app.py
@@ -102,14 +102,4 @@ def logout():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-        
-    #     # Kullanıcı oluşturma
-    #     if not User.query.filter_by(username='admin').first():
-    #         hashed_password = generate_password_hash('password123', method='pbkdf2:sha256')
-    #         new_user = User(username='admin', password=hashed_password)
-    #         db.session.add(new_user)
-    #         db.session.commit()
-        
     app.run(debug=False, host='0.0.0.0', port=5000)
