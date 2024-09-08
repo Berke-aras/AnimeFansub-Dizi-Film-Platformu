ac25a8e538898bc68d5c21f33478ae5db0c8e21d
Berke-aras <berkearastaman@outlook.com>
2024-09-08 10:53:00 +0300

    DİSQUS

--- app.py
+++ app.py
@@ -174,5 +174,5 @@ def logout():
 # Rastgele güçlü bir anahtar
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
-    # app.run(debug=True, host='0.0.0.0', port=5000)
+    # app.run(debug=False, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5000)
